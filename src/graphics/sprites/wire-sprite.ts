2aeacba6116510720846a777d0d00fc4802de4f9
Tyler Voss <tVoss42@gmail.com>
2019-08-04 09:55:52 -0700

    Made what I wanted to

--- src/graphics/sprites/wire-sprite.ts
+++ src/graphics/sprites/wire-sprite.ts
@@ -14,27 +14,36 @@ export class WireSprite extends Entity {
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
-        // just one for now
-        const input = this._wire.inputs.keys().next().value
-        const output = this._wire.outputs.keys().next().value
-
-        const outPos = this._bm.getOutputPos(output)
-        if (!outPos) {
-            return
-        }
-
-        let inPos = this._bm.getInputPos(input)
-        if (!inPos) {
-            inPos = { x: outPos.x + this._scale, y: outPos.y}
-        }
-
         ctx.strokeStyle = this._bm.getSignalColor(this._wire._nextSignal)
         ctx.lineWidth = 2
+        
+        this._wire.outputs.forEach(output => {
+            const outPos = this._bm.getOutputPos(output)
+            if (outPos === null) {
+                console.warn(`Missing output pos for wire ${this._wire.name}`)
+                return
+            }
+            if (this._wire.inputs.size === 0) {
+                const inPos = { x: outPos.x + this._scale, y: outPos.y }
+                ctx.beginPath()
+                ctx.moveTo(inPos.x, inPos.y)
+                ctx.lineTo(outPos.x, outPos.y)
+                ctx.stroke()
+                return 
+            }
+            this._wire.inputs.forEach(input => {
+                let inPos = this._bm.getInputPos(input)
+                if (!inPos) {
+                    console.warn(`Missing input post for wire ${this._wire.name}`)
+                    return
+                }
 
-        ctx.beginPath()
-        ctx.moveTo(inPos.x, inPos.y)
-        ctx.lineTo(outPos.x, outPos.y)
-        ctx.stroke()
+                ctx.beginPath()
+                ctx.moveTo(inPos.x, inPos.y)
+                ctx.lineTo(outPos.x, outPos.y)
+                ctx.stroke()
+            })
+        })
     }
 
     cointainsPoint(point: Point, ctx: CanvasRenderingContext2D): Entity | null {
--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,24 @@ const context = computeCanvas.getContext('2d') as CanvasRenderingContext2D
 const aElem = $('#a')
 const bElem = $('#b')
 
-const aSrc = new Source(() => aElem.prop('checked'), 'b')
+const aSrc = new Source(() => aElem.prop('checked'), 'a')
 const bSrc = new Source(() => bElem.prop('checked'), 'b')
 
-const nandGate = new NandGate('0')
-const orGate = new OrGate('1')
-const notGate = new NotGate('2')
+const flip0 = new NotGate('flip0')
+const flip1 = new NotGate('flip1')
+const flip2 = new NotGate('flip2')
+const and = new AndGate('end')
 
-const chips = [aSrc, bSrc, nandGate, orGate, notGate]
+const chips = [aSrc, flip0, flip1, flip2, and]
 
-const a = new Wire('a_out', [aSrc.x], [nandGate.a])
-const b = new Wire('b_out', [bSrc.x], [nandGate.b])
+const a = new Wire('a_out', [aSrc.x], [flip0.a, and.a])
+const b = new Wire('flip0_out', [flip0.x], [flip1.a])
+const c = new Wire('flip1_out', [flip1.x], [flip2.a])
+const d = new Wire('flip2_out', [flip2.x], [and.b])
 
-const andOut = new Wire('nand_out', [nandGate.x], [notGate.a])
-const notOut = new Wire('not_out', [notGate.x])
+const final = new Wire('final_out', [and.x], [])
 
-const wires = [a, b, andOut, notOut]
+const wires = [a, b, c, d, final]
 
 const bm = new BoardManager()
 wires.forEach(w => bm.addWire(w))
